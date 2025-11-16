e61cf17d2cd96c8eccdc4b6fcfbfb08f62af317f
DumithLusena <dumithlusena123@gmail.com>
2025-11-16 22:17:45 +0530

    feat: implement dashboard components including AppHeader, DoctorsAgentCard, DoctorsAgentList, HistoryList, and data for AI Doctor Agents

---
+++ app/(routes)/dashboard/_components/DoctorsAgentList.tsx
@@ -0,0 +1,22 @@
+import { AIDoctorAgents } from '@/shared/list'
+import { div } from 'motion/react-client'
+import React from 'react'
+import DoctorsAgentCard from './DoctorsAgentCard'
+
+function DoctorsAgentList() {
+  return (
+    <div className='mt-10'>
+        <h2 className='font-bold text-xl'>AI Specialist Doctors Agent</h2>
+        
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 mt-5'>
+            {AIDoctorAgents.map((doctor,index) => (
+                <div key={index}>
+                    <DoctorsAgentCard doctorAgent={doctor} />
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default DoctorsAgentList
